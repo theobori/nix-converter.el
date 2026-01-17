2058c206e4f376f9d96fcca3b9b3a3f5fed66bbc
Théo Bori <theobori@disroot.org>
2026-01-17 05:04:57 +0100

    Update functions documentation and improved functions parameters

--- nix-converter.el
+++ nix-converter.el
@@ -59,16 +59,16 @@
   "Default nix-converter argument surrounder.")
 
 (defun nix-converter--make-safe-argument (argument &optional surrounder)
-  "Description."
+  "Make a command line ARGUMENT safe by surrounding it with a SURROUNDER."
   (setq surrounder (or surrounder nix-converter-default-argument-surrounder))
   (concat surrounder argument surrounder))
 
-(defun nix-converter--build-command-line-flags (file from-nix language &rest flags)
+(defun nix-converter--build-command-line-flags (language &optional file from-nix &rest flags)
   "Build the nix-converter command line flags, it returns a formatted
-string from FLAGS which is a list of string."
+command line as string representing each parameters bound to its
+argument."
   (unless (member language nix-converter-languages)
     (error "LANGUAGE must be one of the following values `%S" nix-converter-languages))
-  ;; Please keep the same inserion order as the function arguments.
   (let ((flag-list nil))
     (when file
       (setq flag-list (append flag-list (list "--filename" (nix-converter--make-safe-argument file)))))
@@ -80,12 +80,12 @@ string from FLAGS which is a list of string."
       (setq flag-list (append flag-list nix-converter-command-line-flags)))
     (string-join flag-list " ")))
 
-(defun nix-converter--build-command-line (file content from-nix language &rest flags)
-  "Description."
+(defun nix-converter--build-command-line (language &optional file content from-nix &rest flags)
+  "Build the full nix-converter command line."
   (when (and (string-empty-p file) (string-empty-p content))
     (error "FILE or CONTENT must be non-empty strings"))
   (let* ((flags (apply
-		 'nix-converter--build-command-line-flags file from-nix language flags))
+		 'nix-converter--build-command-line-flags language file from-nix flags))
 	 (command-line-list (list nix-converter-executable flags)))
     (when content
       (setq command-line-list
@@ -94,12 +94,13 @@ string from FLAGS which is a list of string."
 	     command-line-list)))
     (string-join command-line-list " ")))
 
-(defun nix-converter--run (file content from-nix language &rest flags)
-  "Description."
+(defun nix-converter--run (language &optional file content from-nix &rest flags)
+  "Run nix-converter with the main CLI arguments and returns the output as
+string."
   (unless nix-converter-executable
     (error "Unable to find nix-converter executable"))
   (let* ((command-line (apply
-			'nix-converter--build-command-line file content from-nix language flags))
+			'nix-converter--build-command-line language file content from-nix flags))
 	 ;; Here we are using the `shell-command-to-string' function
 	 ;; because a shell interpreter is needed. Indeed, the echo
 	 ;; function could be needed if the is non-nil.
@@ -108,13 +109,27 @@ string from FLAGS which is a list of string."
       (error "The following error occured during the nix-converter execution: %s" output))
     output))
 
-(defun nix-converter-run-with-file (file from-nix language &rest flags)
-  "Description."
-  (apply 'nix-converter--run file nil from-nix language flags))
+(defun nix-converter-run-with-file (file language &optional from-nix &rest flags)
+  "Run nix-converter with a filepath as input and returns the output as
+string.
+FILE is the filepath.
+LANGUAGE is the second language, see `nix-converter-languages' for more defails.
+If FROM-NIX is non-nil, it will convert from Nix to LANGUAGE.
+FLAGS are additional command line flags."
+  (apply 'nix-converter--run language file nil from-nix flags))
 
-(defun nix-converter-run-with-content (content from-nix language &rest flags)
-  "Description."
-  (apply 'nix-converter--run nil content from-nix language flags))
+(defun nix-converter-run-with-content (content language &optional from-nix &rest flags)
+  "Run nix-converter with the content to convert as input and returns the
+output as string.
+CONTENT is the content to convert.
+LANGUAGE is the second language, see `nix-converter-languages' for more defails.
+If FROM-NIX is non-nil, it will convert from Nix to LANGUAGE.
+FLAGS are additional command line flags."
+  (apply 'nix-converter--run language nil content from-nix flags))
+
+(defun nix-converter-convert-region (language &optional from-nix)
+  ;; TODO
+  )
 
 (provide 'nix-converter)
 
